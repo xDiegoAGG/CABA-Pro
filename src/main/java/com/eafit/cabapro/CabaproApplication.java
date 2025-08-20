97058c2506d7f8eeb6d577c4b186e5608d847d99
Diego Andres Gonzalez Graciano <dagonzal11@eafit.edu.co>
2025-08-20 11:55:39 -0500

    first commit

---
+++ src/main/java/com/eafit/cabapro/CabaproApplication.java
@@ -0,0 +1,13 @@
+package com.eafit.cabapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eafit/cabapro/CabaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eafit.cabapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
